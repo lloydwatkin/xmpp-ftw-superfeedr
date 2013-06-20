1c78c97ef3721c0e50eae1bfcd063e561d10cdc1
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-20 21:54:33 +0100

    Fix library reference

---
+++ test/lib/superfeedr.js
@@ -0,0 +1,28 @@
+var should  = require('should')
+  , Superfeedr = require('../../lib/superfeedr')
+  , ltx     = require('ltx')
+  , helper  = require('../helper')
+  , xhtmlIm = require('xmpp-ftw/lib/utils/xep-0071')
+
+describe('Superfeedr', function() {
+
+    var superfeedr, socket, xmpp, manager
+
+    before(function() {
+        socket = new helper.Eventer()
+        xmpp = new helper.Eventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            }
+        }
+        superfeedr = new Superfeedr()
+        superfeedr.init(manager)
+    })
+
+})
